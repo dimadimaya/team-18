9f1d1a3fcfe1833f8088911c469aa70d9728615b
dimadimaya <103739636+dimadimaya@users.noreply.github.com>
2022-08-16 08:37:35 +0000

    Deploying to gh-pages from @ dimadimaya/team-18@66f81000bb3b3251f018b5ac53cacfddfc8d2166 🚀

---
+++ index.cf290be4.js
@@ -0,0 +1,2 @@
+var galleryTilesSet=document.querySelectorAll(".gallery__img");function getRandomInt(t){return Math.floor(Math.random()*t)}[].forEach.call(galleryTilesSet,(function(t){t.style.animationDuration=getRandomInt(6)+4+"s"}));
+//# sourceMappingURL=index.cf290be4.js.map
